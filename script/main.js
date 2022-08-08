d1709e34fec0be88aa3fe6a5d687a418edb1a3f3
swygax <kennykysv@gmail.com>
2022-08-08 00:32:24 -0300

    Initial Commit

---
+++ script/main.js
@@ -0,0 +1,14 @@
+const imgPerfil = window.document.getElementById("perfil")
+const selectGender = window.document.getElementsByName("selectGender")
+const inputAge = window.document.getElementById("age")
+const images = ["image/female/girl.jpg",
+                "image/female/woman.jpg",
+                "image/female/old_woman.jpg",
+                "image/male/boy.jpg",
+                "image/male/man.jpg",
+                "image/male/old_man.jpg"
+]
+
+function loadComponents() {
+    // create function here
+}
